d121295a072329641c12d398a8f95ecf6dfd02e5
radmanrezaei <140076640+radmanrezaei@users.noreply.github.com>
2023-07-20 14:54:43 +0200

    Update index.js
    update contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x852c99545781b3960cf55f92a4fda38becd7f9be";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
